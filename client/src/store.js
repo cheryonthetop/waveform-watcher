de9a7a3912c0d5490bc265a50f6f2831d50a12eb
Yingfan Chen <cyf731405@hotmail.com>
2020-07-09 13:02:24 +0800

    disable redux chrome dev tool in production

--- client/src/store.js
+++ client/src/store.js
@@ -6,13 +6,23 @@ const initialState = {};
 
 const middleware = [thunk];
 
-const store = createStore(
-  rootReducer,
-  initialState,
-  compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
-);
+let store;
+if (process.env.NODE_ENV === "development") {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(
+      applyMiddleware(...middleware),
+      window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+  );
+} else {
+  store = createStore(
+    rootReducer,
+    initialState,
+    compose(applyMiddleware(...middleware))
+  );
+}
 
 export default store;
