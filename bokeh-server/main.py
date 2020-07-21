4fdfb8c53e4506b88430bd91fbc6bf42d4009716
Yingfan Chen <cyf731405@hotmail.com>
2020-07-21 17:49:12 +0800

    bokeh server folder; data caching folder

---
+++ bokeh-server/main.py
@@ -0,0 +1,149 @@
+
+import os
+from bokeh.layouts import column, gridplot, row
+from bokeh.plotting import figure, curdoc
+from bokeh.models import ColumnDataSource, CustomJS, OpenURL, TapTool, Button, TextInput, MultiSelect, Div
+import holoviews as hv
+import pymongo
+
+hv.extension("bokeh")
+
+# Fixed Parameter in Plots for Event Selections
+DIMS = [
+    ["cs1", "cs2"],
+    ["z","r" ],
+    ["e_light", 'e_charge'],
+    ["e_light", 'e_ces'],
+    ["drift_time", "n_peaks"],
+]
+COLORS = hv.Cycle('Category10').values
+TOOLS=['box_select', 'lasso_select', 'box_zoom', 'pan', 
+       'wheel_zoom', 'hover', 'tap', 'save', 'reset']
+
+# The URL of the app (front-end)
+if os.environ.get("ENV") == "production":
+    APP_URL = os.environ.get("APP_URL", None)  
+else: 
+    APP_URL = "http://localhost:3000"
+if (APP_URL == None):
+    print("Env Variable APP_URL Is Not Set")
+
+# Connect to MongoDB
+APP_DB_URI = os.environ.get("APP_DB_URI", None)
+if (APP_DB_URI == None):
+    print("MongoDB Connection String Not Set")    
+    
+my_db = pymongo.MongoClient(APP_DB_URI)["waveform"]
+my_request = my_db["request"]
+my_events = my_db["events"]
+
+##### DB routine helpers
+def cache_waveform_request(run_id, event_id):
+    print("cacheing waveform for run ", run_id, "and event ", event_id)
+    document = my_request.find_one({"run_id" : run_id, "event_id" : event_id})
+    # cache to request if not already in it
+    if (document == None):
+        post = {"status" : "new", "run_id" : run_id, "event_id" : event_id, "request": "waveform"}
+        my_request.insert_one(post) # insert mongo document into 'fetch'
+
+def get_events(run_id):
+    events = None
+    document = my_events.find_one({"run_id" : run_id})
+    if (document):
+        if (document["events"]):
+            events = pd.DataFrame(pickle.loads(document["events"]))
+        else:
+            return document["msg"]
+    return events
+
+def wait_for_events(run_id):
+    # wait for 5 minutes max
+    endtime = datetime.datetime.now() + datetime.timedelta(0, 60 * 5)
+    while True:
+        events = get_events(run_id)
+        if (isinstance(events, pd.DataFrame)):
+            print("retrieved events")
+            return events
+        print("Still Getting Events...")
+        # Wait for waveform to be loaded
+        time.sleep(10)
+        if (datetime.datetime.now() >= endtime):
+            return "Get Events Timeout. Please Try Again."
+
+def render_events(run_id, events):
+    source = ColumnDataSource(events)
+
+    # Other components (or models or widgets in Bokeh terms)
+    text_input = TextInput(value=run_id, title="Events for Run: ")
+    multi_select = MultiSelect(title="Selected Events:", value=[], options=[])
+    multi_select.width=600
+
+    def callback_select(attr, old, new):
+        print("selected data")
+        inds = new
+        print(inds)
+        for i in range(0, len(inds)):
+            event = str(source.data['event_number'][inds[i]])
+            if event not in multi_select.options:
+                multi_select.options.append(str(event))
+            
+    source.selected.on_change('indices', callback_select)
+
+    def callback_value_selected(attr, old, new):
+        print("new value callback")
+        value = new
+        if len(value) != 0:
+            button.disabled = False
+
+    multi_select.on_change("value", callback_value_selected)
+    plots = []
+    # use the "color" column of the Csource to complete the URL
+    # e.g. if the glyph at index 10 is selected, then @color
+    # will be replaced with source.data['color'][10]
+    url = "{APP_URL}/waveform/{run_id}/@event_number/".format(APP_URL=APP_URL, run_id=run_id)
+    for color,dim in zip(COLORS, DIMS):
+        p = figure(tools=TOOLS, x_axis_label=dim[0], y_axis_label=dim[1])
+        p.circle(dim[0], dim[1], source=source, alpha=0.6, color=color)
+        taptool = p.select(type=TapTool)
+        taptool.callback = OpenURL(url=url)
+        plots.append(p)
+    # make a grid
+    grid = gridplot([plots[:3], plots[3:]], plot_width=300, plot_height=300)
+            
+    # create a callback that will cache waveform
+    def callback_button():
+        events = multi_select.value
+        events = [int(event) for event in events]
+        for event in events:
+            cache_waveform_request(run_id, event)    
+
+    # add a button widget and configure with the call back
+    button = Button(label="Cache Waveform for Selected Events")
+    button.on_click(callback_button)
+    button.disabled = len(multi_select.value) == 0
+    button.align="center"
+    button.height=40
+    button.margin=(10,0,0,0)
+
+    # put the button and plot in a layout and add to the document
+    curdoc().add_root(column(text_input, row(multi_select, button), grid))
+
+###### Appends model to document
+
+# request.arguments is a dict that maps argument names to lists of strings,
+# e.g, the query string ?N=10 will result in {'N': [b'10']}
+args = curdoc().session_context.request.arguments
+
+# run_id = args.get('run_id')[0]
+# print(run_id)  
+run_id = "170204_1710"
+
+events = wait_for_events(run_id)
+if (isinstance(events, str)):
+    # An error string returned
+    div = Div(text=events)
+    div.align="center"
+    div.sizing_mode="stretch_both"
+    curdoc().add_root(column(div))
+else:
+    render_events(run_id, events)
---
+++ data-caching/cache_service.py
@@ -0,0 +1,126 @@
+import pickle
+import threading
+from threading import Thread
+import os
+from bson.binary import Binary
+import straxen
+import holoviews as hv
+import pymongo
+import datetime
+import time
+import bokeh
+import pandas as pd
+import traceback
+from holoviews_waveform_display import waveform_display
+
+# Bokeh backend setup
+hv.extension("bokeh")
+renderer = hv.renderer('bokeh') # Bokeh Server
+lock = threading.Lock() # A Lock to address race condition
+
+# Get the number of processors/cores available in the system
+cpu_count = os.cpu_count()
+print("Number of processors/cores available in the system: ", cpu_count)
+
+APP_DB_URI = os.environ.get("APP_DB_URI", None)
+if (APP_DB_URI == None):
+    print("MongoDB Connection String Not Set")
+my_db = pymongo.MongoClient(APP_DB_URI)["waveform"]
+my_request = my_db["request"]
+my_waveform = my_db["waveform"]
+my_events = my_db["events"]
+dims = ["cs1", "cs2", "z", "r", "e_light", "e_charge", 
+        "e_light", "e_ces", "drift_time", "n_peaks", "event_number"]
+st = straxen.contexts.xenon1t_dali()
+
+def load_waveform(run_id, event_id):
+    df = st.get_array(run_id, "event_info")
+    event = df[int(event_id)]
+    waveform = waveform_display(context=st, run_id=run_id, time_within=event)
+    lock.acquire()
+    try:
+        print("Waveform is: ", waveform)
+        waveform = renderer.server_doc(waveform).roots[-1]
+    finally:
+        lock.release()
+    waveform = bokeh.embed.json_item(waveform)
+    return waveform
+
+def load_events(run_id):
+    events = st.get_df(run_id, 'event_info')
+    new_events = pd.DataFrame()
+    for dim in dims:
+        new_events[dim] = events[dim]
+    return Binary(pickle.dumps(new_events))
+    
+def cache_events(run_id, events, msg):
+    post = {"run_id" : run_id, "events": events, "msg": msg}
+    document = my_events.find_one(post)
+    if (document == None):
+        my_events.insert_one(post)
+
+def cache_waveform(run_id, event_id, waveform, msg):
+    post = {"event_id" : event_id, "run_id" : run_id, "waveform": waveform, "msg": msg}
+    document = my_waveform.find_one(post)
+    if (document == None):
+        my_waveform.insert_one(post)
+
+def process_waveform(run_id, event_id):
+    print("starts processing waveform for run ", run_id, " and ", event_id)
+    try:
+        waveform = load_waveform(run_id, event_id)
+        cache_waveform(run_id, event_id, waveform, "")
+    except Exception as e:
+        print("An exception occured ")
+        traceback.print_tb(e.__traceback__)
+        if hasattr(e, "message"):
+            cache_waveform(run_id, event_id, None, e.message)
+        else:
+            cache_waveform(run_id, event_id, None, "Data Not Available")
+    my_request.delete_one({"status": "use", "event_id": event_id, "run_id": run_id})
+    print("Just cached the waveform for run ", run_id, "and event ", event_id)
+    
+def process_events(run_id):
+    print("starts processing events for run ", run_id)
+    try:
+        events = load_events(run_id)
+        cache_events(run_id, events, "")
+    except Exception as e:
+        print("An exception occured ", e)
+        traceback.print_tb(e.__traceback__)
+        if hasattr(e, "message"):
+            cache_events(run_id, None, e.message)
+        else:
+            cache_events(run_id, None, "Data Not Available")
+    my_request.delete_one({"status": "use", "run_id": run_id})
+    print("Just cached the events for run ", run_id)
+    
+def fetch_request():
+    while True:
+        document = my_request.find_one_and_update({"status": "new"}, 
+                                                  {"$set" : {"status": "use"}})
+        while (document):
+            request = document["request"]
+            if (request == "waveform"):
+                run_id = document["run_id"]
+                event_id = document["event_id"]
+                process_waveform(run_id, event_id)
+            else:
+                run_id = document["run_id"]
+                process_events(run_id)
+            document = my_request.find_one_and_update({"status": "new"}, 
+                                            {"$set" : {"status": "use"}})
+        # Sleep the app if no new requests
+        time.sleep(10)
+
+if __name__ == "__main__":
+    print("Service starts now: ", datetime.datetime.now())
+    threads = []
+    start_time = datetime.datetime.now()
+    for i in range(0, min(8, cpu_count)):
+        t = Thread(target=fetch_request, daemon=True)
+        threads.append(t)
+        t.start()
+    # Main thread sleeps for 1 hour and terminates
+    # Daemonic threads are terminated automatically
+    time.sleep(60 * 60)
