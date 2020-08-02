4d2d938528b893499bf33236952f866c1222bc4e
Yingfan Chen <cyf731405@hotmail.com>
2020-08-02 10:07:31 +0800

    helpers

---
+++ bokeh-server/helpers.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import datetime
+import time
+import pickle
+import pymongo
+import os
+
+# Connect to MongoDB
+APP_DB_URI = os.environ.get("APP_DB_URI", None)
+if APP_DB_URI == None:
+    print("MongoDB Connection String Not Set")
+
+my_db = pymongo.MongoClient(APP_DB_URI)["waveform"]
+my_request = my_db["request"]
+my_events = my_db["events"]
+my_waveform = my_db["waveform"]
+
+##### DB routine helpers
+def cache_waveform_request(run_id, event_id):
+    """
+    Inserts a request into the request collection that
+    asks for a waveform
+
+    Args:
+        run_id (str): Run ID of the run
+        event_id (str): Event ID of the event
+    """
+    print("cacheing waveform for run ", run_id, "and event ", event_id)
+    request = my_request.find_one({"run_id": run_id, "event_id": event_id})
+    waveform = my_waveform.find_one({"run_id": run_id, "event_id": event_id})
+    # cache to request if not already in it
+    if request == None and waveform == None:
+        post = {
+            "status": "new",
+            "run_id": run_id,
+            "event_id": event_id,
+            "request": "waveform",
+        }
+        my_request.insert_one(post)  # insert mongo document into 'fetch'
+
+
+def get_events_from_cache(run_id):
+    """
+    Gets events from the cache
+
+    Args:
+        run_id (str): Run ID of the run
+
+    Returns:
+        DataFrame: All events from the run with DIMS as the fields
+    """
+    events = None
+    document = my_events.find_one({"run_id": run_id})
+    if document:
+        if document["events"]:
+            events = pd.DataFrame(pickle.loads(document["events"]))
+        else:
+            return document["msg"]
+    return events
+
+
+def wait_for_events(run_id):
+    """
+    Repeatedly waits for events to be returned from the cache       
+
+    Args:
+        run_id (str): Run ID of the run
+
+    Returns:
+        DataFrame/str: A dataframe with all events from the run, or
+        an error message if the events are not retrieved, or a timeout message
+        to indicate the waveform has not yet been found in the cache for 5 minutes
+    """
+    # wait for 5 minutes max
+    endtime = datetime.datetime.now() + datetime.timedelta(0, 60 * 5)
+    while True:
+        events = get_events_from_cache(run_id)
+        if isinstance(events, pd.DataFrame):
+            print("retrieved events")
+            return events
+        print("Still Getting Events...")
+        # Wait for waveform to be loaded
+        time.sleep(10)
+        if datetime.datetime.now() >= endtime:
+            return "Get Events Timeout. Please Try Again."
--- bokeh-server/sourcing.py
+++ bokeh-server/sourcing.py
@@ -17,19 +17,13 @@ from bokeh.models import (
 )
 import holoviews as hv
 import pymongo
-import datetime
-import pickle
 import pandas as pd
-import time
 import threading
 from tornado import gen
 from functools import partial
+from helpers import cache_waveform_request, get_events_from_cache, wait_for_events
 
 hv.extension("bokeh")
-# Current document
-doc = curdoc()
-run_id = None
-
 # Fixed Parameter in Plots for Event Selections
 DIMS = [
     ["cs1", "cs2"],
@@ -69,76 +63,9 @@ my_request = my_db["request"]
 my_events = my_db["events"]
 my_waveform = my_db["waveform"]
 
-##### DB routine helpers
-def cache_waveform_request(run_id, event_id):
-    """
-    Inserts a request into the request collection that
-    asks for a waveform
-
-    Args:
-        run_id (str): Run ID of the run
-        event_id (str): Event ID of the event
-    """
-    print("cacheing waveform for run ", run_id, "and event ", event_id)
-    request = my_request.find_one({"run_id": run_id, "event_id": event_id})
-    waveform = my_waveform.find_one({"run_id": run_id, "event_id": event_id})
-    # cache to request if not already in it
-    if request == None and waveform == None:
-        post = {
-            "status": "new",
-            "run_id": run_id,
-            "event_id": event_id,
-            "request": "waveform",
-        }
-        my_request.insert_one(post)  # insert mongo document into 'fetch'
-
-
-def get_events_from_cache(run_id):
-    """
-    Gets events from the cache
-
-    Args:
-        run_id (str): Run ID of the run
-
-    Returns:
-        DataFrame: All events from the run with DIMS as the fields
-    """
-    events = None
-    document = my_events.find_one({"run_id": run_id})
-    if document:
-        if document["events"]:
-            events = pd.DataFrame(pickle.loads(document["events"]))
-        else:
-            return document["msg"]
-    return events
-
-
-def wait_for_events(run_id):
-    """
-    Repeatedly waits for events to be returned from the cache       
-
-    Args:
-        run_id (str): Run ID of the run
-
-    Returns:
-        DataFrame/str: A dataframe with all events from the run, or
-        an error message if the events are not retrieved, or a timeout message
-        to indicate the waveform has not yet been found in the cache for 5 minutes
-    """
-    # wait for 5 minutes max
-    endtime = datetime.datetime.now() + datetime.timedelta(0, 60 * 5)
-    while True:
-        events = get_events_from_cache(run_id)
-        if isinstance(events, pd.DataFrame):
-            print("retrieved events")
-            return events
-        print("Still Getting Events...")
-        # Wait for waveform to be loaded
-        time.sleep(10)
-        if datetime.datetime.now() >= endtime:
-            return "Get Events Timeout. Please Try Again."
-
 ###### Appends model to document
+doc = curdoc()
+run_id = None
 
 # request.arguments is a dict that maps argument names to lists of strings,
 # e.g, the query string ?N=10 will result in {'N': [b'10']}
@@ -150,7 +77,7 @@ try:
 except:
     print("no run id")
 
-dims = [
+columns = [
     "cs1",
     "cs2",
     "z",
@@ -163,9 +90,9 @@ dims = [
     "n_peaks",
     "event_number",
 ]
-default = {}
-for dim in dims:
-    default[dim] = []
+default = pd.DataFrame()
+for col in columns:
+    default[col] = [0]
 source = ColumnDataSource(data=default)
     
 def callback_select(attr, old, new):
@@ -330,15 +257,16 @@ print("Inserted layout")
 
 @gen.coroutine
 def update(events):
-    source.stream(events)
+    # source.stream(events)
+    source = events
 
 def blocking_task():
     events = wait_for_events(run_id)
-    new_events = {}
-    for dim in dims:
-        new_events[dim] = events[dim]
+    # new_events = {}
+    # for dim in dims:
+    #     new_events[dim] = [1, 2, 3, 4, 5]
     # but update the document from callback
-    doc.add_next_tick_callback(partial(update, events=new_events))
+    doc.add_next_tick_callback(partial(update, events=events))
 
 thread = threading.Thread(target=blocking_task)
 thread.start()
